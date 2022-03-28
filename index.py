1ceddfc4428bd022d27be5abce3dec1fda0a9963
pdf114514 <haruton321@outlook.jp>
2022-03-28 18:48:53 +0900

    Update index.py

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import m3u8
 from zlib import decompress
 import requests
 import json
+import asyncio
 
 with open("config.json", "r") as f:
     data = json.load(f)
@@ -30,6 +31,47 @@ def get_radioinfo(resourceId: str) -> dict:
     else:
         return json.loads(decompress(data[8:len(data)]).decode("utf-8"))
 
+async def owners_only(interaction: discord.Interaction) -> bool:
+    return interaction.user.id in owners
+
+class RadioButtons(discord.ui.View):
+    def __init__(self, voice_client: discord.VoiceClient):
+        super().__init__()
+        self.voice_client: discord.VoiceClient = voice_client
+        self.nextreq: bool = False
+        self.playing: bool = True
+        self.stopped: bool = False
+    
+    @discord.ui.button(label="⏭", style=discord.ButtonStyle.green)
+    async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not await owners_only(interaction):
+            await interaction.response.send_message("You don't have permission to use the command.", ephemeral=True)
+        else:
+            self.nextreq = True
+            self.voice_client.stop()
+            await interaction.response.edit_message(view=self)
+    
+    @discord.ui.button(label="⏸", style=discord.ButtonStyle.grey)
+    async def play_or_pause(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not await owners_only(interaction):
+            await interaction.response.send_message("You don't have permission to use the command.", ephemeral=True)
+        else:
+            self.playing = not self.playing
+            button.label = ("⏸" if self.playing else "▶")
+            self.voice_client.resume() if self.playing else self.voice_client.pause()
+            await interaction.response.edit_message(view=self)
+
+    @discord.ui.button(label="⏹", style=discord.ButtonStyle.red)
+    async def stop(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if not await owners_only(interaction):
+            await interaction.response.send_message("You don't have permission to use the command.", ephemeral=True)
+        else:
+            self.playing = False
+            self.stopped = True
+            self.voice_client.stop()
+            await self.voice_client.disconnect()
+            await interaction.response.edit_message(content="Bye!", embed=None, view=None)
+
 async def play_radio(VoiceChannel: discord.VoiceChannel, TextChannel: discord.TextChannel, language: str):
     index = 0
     voice_client: discord.VoiceClient = await VoiceChannel.connect(reconnect=False)
@@ -52,31 +94,34 @@ async def play_radio(VoiceChannel: discord.VoiceChannel, TextChannel: discord.Te
                     break
         master_audio = master_m3u8.media
 
+        buttons = RadioButtons(voice_client)
         embed = discord.Embed(title=radio["title"], description=f"resourceID: "+radio["resourceID"])
         embed.set_image(url=radio["stationImage"])
-        await msg.edit(embed=embed)
+        await msg.edit(embed=embed, view=buttons)
 
-        voice_client.play(discord.FFmpegPCMAudio(f"{master_uri}/{master_audio[0].uri}"))
-        while voice_client.is_playing():
-            await __import__('asyncio').sleep(5)
+        voice_client.play(discord.FFmpegPCMAudio(f"{master_uri}/{master_audio[0].uri}", before_options="-reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 8", options="-vn"))
+        while voice_client.is_playing() or not buttons.playing:
+            await asyncio.sleep(.5)
+            if buttons.nextreq:
+                buttons.nextreq = False
+                break
+        if buttons.stopped:
+            return
 
-        index=+1
+        index+=1
         if index >= len(radios):
             index = 0
-
-async def owners_only(interaction: discord.Interaction) -> bool:
-    return interaction.user.id in owners
 
 @tree.command(name="start", description="Start Playing Radio")
 @discord.app_commands.check(owners_only)
 async def command_start(interaction: discord.Interaction, play_in: discord.VoiceChannel, info: discord.TextChannel, lang: str):
-    radio_tasks.append(__import__('asyncio').create_task(play_radio(play_in, info, lang)))
-    await interaction.response.send_message("Started playing radio.")
+    radio_tasks.append(asyncio.create_task(play_radio(play_in, info, lang)))
+    await interaction.response.send_message("Started playing radio.", ephemeral=True)
 
 @command_start.error
 async def command_start_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
     if isinstance(error, discord.app_commands.CheckFailure):
-        await interaction.response.send_message("You don't have permission to use the command.")
+        await interaction.response.send_message("You don't have permission to use the command.", ephemeral=True)
     else:
         print(str(error))
         await interaction.response.send_message(f"Error\n```\n{str(error)}```")
